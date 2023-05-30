7116dfd49baf13a357e1cbdd06eb4dc6a2054ee2
00162317 <hernandez.roberto014@gmail.com>
2023-05-30 14:04:05 -0600

    Cambio en comprometido

--- COMPROMETIDO.sql
+++ COMPROMETIDO.sql
@@ -2,4 +2,5 @@ SELECT T0."ItemCode", T0."ItemName",T1."IsCommited" Comprometido,T2."WhsCode"
 FROM OITM T0  
 INNER JOIN OITW T1 ON T0."ItemCode" = T1."ItemCode" 
 INNER JOIN OWHS T2 ON T1."WhsCode" = T2."WhsCode" 
+WHERE T1."IsCommited"!=0
 ORDER BY T1."IsCommited" DESC
